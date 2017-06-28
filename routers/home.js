394c169995c0248925c837eb3f1f59748c8ced11
Cas Burggraaf <casburggraaf@gmail.com>
2017-06-28 23:33:35 +0200

    finished stylings chats

--- routers/home.js
+++ routers/home.js
@@ -7,7 +7,7 @@ router.get("/", function(req, res){
   if(req.session.ingelogd == true){
     req.getConnection(function(err, connection){
         if(err) return next(err);
-        connection.query(`SELECT gebruiker.* FROM gebruiker WHERE (favartist1 = "${req.session.user.favartist1}" OR favartist2 = "${req.session.user.favartist1}" OR favartist3 = "${req.session.user.favartist1}" OR favartist1 = "${req.session.user.favartist2}" OR favartist2 = "${req.session.user.favartist2}" OR favartist3 = "${req.session.user.favartist2}" OR favartist1 = "${req.session.user.favartist3}" OR favartist2 = "${req.session.user.favartist3}" OR favartist3 = "${req.session.user.favartist3}") AND id != ${req.session.user.ID} AND geslacht = "${req.session.user.zoekgeslacht}" `, function(err, result) {
+        connection.query(`SELECT gebruiker.* FROM gebruiker WHERE (favartist1 = "${req.session.user.favartist1}" OR favartist2 = "${req.session.user.favartist1}" OR favartist3 = "${req.session.user.favartist1}" OR favartist1 = "${req.session.user.favartist2}" OR favartist2 = "${req.session.user.favartist2}" OR favartist3 = "${req.session.user.favartist2}" OR favartist1 = "${req.session.user.favartist3}" OR favartist2 = "${req.session.user.favartist3}" OR favartist3 = "${req.session.user.favartist3}") AND id != ${req.session.user.ID}`, function(err, result) {
         //connection.query(`SELECT gebruiker.* FROM gebruiker WHERE (favartist1 = "${req.session.user.favartist1}" OR favartist2 = "${req.session.user.favartist1}" OR favartist3 = "${req.session.user.favartist1}" OR favartist1 = "${req.session.user.favartist2}" OR favartist2 = "${req.session.user.favartist2}" OR favartist3 = "${req.session.user.favartist2}") AND id != ${req.session.user.ID} `, function(err, result) {
           if(err) return next(err);
           res.locals.user = req.session.user;
@@ -48,8 +48,8 @@ router.post("/chat", function(req, res){
   req.getConnection(function(err, connection){
     if(err) return next(err);
     connection.query(`INSERT INTO chats (verzenderID, ontvangerID, bericht) VALUES ('${req.session.user.ID}', '${ontvangerID}', '${req.body.bericht}')`, function(err, result) {
-      console.log(err, result);
-      res.redirect("/");
+      console.log(err);
+      res.redirect(`chat?=${ontvangerID}`);
     });
   });
 });
